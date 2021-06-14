255ade064f8e4fd558d0d142c475e7512d6a3cb1
T4dd3 <giova99taddei@gmail.com>
2021-06-14 12:09:42 +0200

    Riformattazione RegistrazioneController e CodiceDiVerificaController

--- src/dcamaster/gestioneaccount/RegistrazioneController.java
+++ src/dcamaster/gestioneaccount/RegistrazioneController.java
@@ -22,33 +22,39 @@ import dcamaster.model.EntryOperazione;
 import dcamaster.model.TipoDeposito;
 import dcamaster.model.ValutaFiat;
 
-public class RegistrazioneController extends HttpServlet implements IRegistrazione{
-
-	private static final long serialVersionUID = 1L;
+@SuppressWarnings("serial")
+public class RegistrazioneController extends HttpServlet implements IRegistrazione
+{
 	private ICodiceDiVerifica codiceController;
 	private ControllerPersistenza controllerPersistenza;
 	private HttpSession session;
 	private ValutaFiatRepository fiatRepo;
 	
 	@Override
-	public void init(ServletConfig config) throws ServletException {
+	public void init(ServletConfig config) throws ServletException 
+	{
 		super.init(config);
-		//controller persistenza come singleton
-		//this.controllerPersistenza = ControllerPersistenza.getInstance();
+		
+		// Istanza del singleton di Controller Persistenza
+		this.controllerPersistenza = ControllerPersistenza.getInstance();
+		
+		// Creazione Controller per gestione codice verifica
 		this.codiceController = new CodiceDiVerificaController(this);
-		//codiceController.setRegistrazioneController();
+		
 		this.fiatRepo = new ValutaFiatRepository(this.controllerPersistenza);
 	}
 	
-	public ICodiceDiVerifica getCodiceController() {
+	public ICodiceDiVerifica getCodiceController() 
+	{
 		return codiceController;
 	}
 	
+	// Verifico che gli input siano corretti, se lo sono li salvo in sessione e invio il codice
 	@Override
 	public void verificaDatiInseriti(String username, String password, String email, ValutaFiat valutaRiferimento,
-			String apiKey, String apiSecret, TipoDeposito tipoDeposito) {
-		//verifico che gli input siano corretti, se lo sono li salvo in sessione e invio il codice
-		byte[] array = new byte[10];  
+			String apiKey, String apiSecret, TipoDeposito tipoDeposito) 
+	{
+		byte[] array = new byte[32];  
 		new Random().nextBytes(array);
 		String saltPassword = new String(array, Charset.forName("UTF-8"));
 		String data = password + saltPassword;
@@ -62,6 +68,7 @@ public class RegistrazioneController extends HttpServlet implements IRegistrazio
 			e.printStackTrace();
 		}
 		
+		// Salvataggio valori della registrazione in Sessione
 		session.setAttribute("username", username);
 		session.setAttribute("password", hashPassword);
 		session.setAttribute("email", email);
@@ -70,11 +77,14 @@ public class RegistrazioneController extends HttpServlet implements IRegistrazio
 		session.setAttribute("valutaFiat", valutaRiferimento);
 		session.setAttribute("tipoDeposito", tipoDeposito);
 		session.setAttribute("saltPassword", saltPassword);
+		
+		// Invio del codice per mail all'utente
 		this.codiceController.inviaCodice(email);
 	}
 
 	@Override
-	public void registraUtente() {
+	public void registraUtente() 
+	{
 		UserRepository userRepo = new UserRepository(this.controllerPersistenza);
 		String username = (String) session.getAttribute("username");
 		String hashPassword = (String) session.getAttribute("password");
@@ -84,17 +94,22 @@ public class RegistrazioneController extends HttpServlet implements IRegistrazio
 		String apiSecret = (String) session.getAttribute("apiSecret");
 		TipoDeposito tipoDeposito = (TipoDeposito) session.getAttribute("tipoDeposito");
 		String saltPassword = (String) session.getAttribute("saltPassword");
+		
 		try {
-		userRepo.create(username, hashPassword, saltPassword, email, valutaRiferimento, apiKey, apiSecret, tipoDeposito);
-		} catch (PersistenceException e){
+			userRepo.create(username, hashPassword, saltPassword, email, valutaRiferimento, apiKey, apiSecret, tipoDeposito);
+		} catch (PersistenceException e) {
 			//do something
 		}
 		//Inserire entry nei log
 	}
 	
 	@Override
-	public void doPost(HttpServletRequest request, HttpServletResponse response) {
+	public void doPost(HttpServletRequest request, HttpServletResponse response) 
+	{
+		// 
 		session = request.getSession();
+		
+		// Recupero di tutti i valori per la registrazione passati dall'utente
 		String username = (String) request.getAttribute("username");
 		String password = (String) request.getAttribute("password");
 		String email = (String) request.getAttribute("email");
@@ -104,12 +119,13 @@ public class RegistrazioneController extends HttpServlet implements IRegistrazio
 		String apiSecret = (String) request.getAttribute("apiSecret");
 		String nomeTipoDeposito = (String) request.getAttribute("tipoDeposito");
 		TipoDeposito tipoDeposito = TipoDeposito.valueOf("");
-		this.verificaDatiInseriti(username, password, email, 
-					valutaRiferimento, apiKey, apiSecret, tipoDeposito);
+		
+		this.verificaDatiInseriti(username, password, email, valutaRiferimento, apiKey, apiSecret, tipoDeposito);
 		
 	}
 	
-	public void inserisciEntry(EntryOperazione entry) {
+	public void inserisciEntry(EntryOperazione entry) 
+	{
 		this.controllerPersistenza.inserisciEntry(entry);
 	}
 
