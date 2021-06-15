bbff68f4c380b97543f9f74389db2d676756f380
T4dd3 <giova99taddei@gmail.com>
2021-06-15 15:11:09 +0200

    Changed how Registrazione and CodiceDiVerifica controllers are managed; Added the RequestManager as the only servlet in the project = Broker

--- src/dcamaster/gestioneaccount/IRegistrazione.java
+++ src/dcamaster/gestioneaccount/IRegistrazione.java
@@ -1,14 +1,11 @@
 package dcamaster.gestioneaccount;
 
 import dcamaster.model.TipoDeposito;
-import dcamaster.model.ValutaFiat;
 
-public interface IRegistrazione {
-
+public interface IRegistrazione 
+{
 	public void verificaDatiInseriti(String username, String password, String email, 
-			ValutaFiat valutaRiferimento, String apiKey, String apiSecret, TipoDeposito tipoDeposito);
+			String valutaRiferimento, String apiKey, String apiSecret, TipoDeposito tipoDeposito);
 	
 	public void registraUtente();
-	
-	public ICodiceDiVerifica getCodiceController(); //nuovo metodo
 }
