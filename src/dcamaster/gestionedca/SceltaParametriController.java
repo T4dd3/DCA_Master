bf3ef3364520525c53d55dfa2dc1d92ec221f020
adria <adria@LAPTOP-6UROPFFU.home-life.hub>
2021-06-16 17:25:36 +0200

    Removed unused imports

--- src/dcamaster/gestionedca/SceltaParametriController.java
+++ src/dcamaster/gestionedca/SceltaParametriController.java
@@ -1,13 +1,7 @@
 package dcamaster.gestionedca;
 
-import java.io.IOException;
-
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+
 
 import dcamaster.db.ControllerPersistenza;
 import dcamaster.db.PersistenceException;
