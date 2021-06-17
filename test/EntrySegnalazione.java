0e80b1261b4c15d56bbb9e57df0bf1225d3c971c
adria <adria@LAPTOP-6UROPFFU.home-life.hub>
2021-06-17 17:23:52 +0200

    Removed old classes and tests

--- test/EntrySegnalazione.java
+++
@@ -1,9 +0,0 @@
-import java.time.LocalDateTime;
-
-public class EntrySegnalazione extends Entry{
-
-	public EntrySegnalazione(LocalDateTime dataOra, String messaggio) {
-		super(dataOra, messaggio);
-	}
-
-}
